4bd98aaff6e389195d9e083fed244006518672df
aashish2000 <aash.ananth@gmail.com>
2019-10-13 12:05:12 +0530

    Merge writecertificate.py and loadnames.py into single file

--- writeCertificates.py
+++ writeCertificates.py
@@ -1,7 +1,7 @@
+import pandas as pd
 from PIL import Image, ImageDraw, ImageFont
 import img2pdf
 import os
-# create Image object with the input image
 
 def writeimage(template,font,size,coorx,coory,msg,rgb):
 	image = Image.open(template)
@@ -39,3 +39,30 @@ def writeimage(template,font,size,coorx,coory,msg,rgb):
 	image.close()
 
 	f.close() 
+
+
+#Accept User input for Required Details
+
+print("Enter Certificate template Name: ", end="")
+template=input()
+print("Enter CSV Filename: ", end="")
+filename=input()
+print("Enter Column Name containing to Data: ", end="")
+col=input()
+print("Enter TrueType Font Filename: ", end="")
+font=input()
+print("Enter Font Size: ", end="")
+size=int(input())
+print("Enter Space Separated x,y Coordianates of text to written: ", end="")
+coorx,coory=map(int,input().split())
+print("Enter Space Separated RGB values of Text Color: ", end="")
+r,g,b=map(str,input().split())
+
+
+#Open CSV File
+data=pd.read_csv(filename)
+
+for row in data[col]: #Scan names row by row
+
+	writeimage(template,font,size,coorx, coory,row,"rgb("+r+","+g+","+b+")")
+
